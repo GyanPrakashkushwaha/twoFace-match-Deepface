3e195657ae82775371b1b9f4f65cc12129463430
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-08-12 18:14:00 +0530

    working in camera.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from src.save_img import save_uploaded_img_face1 , save_uploaded_img_face2
 from src.similarity import Similarity
 from src.option_menus import option_menus
 from src.pictures_mode import pictures_mode
+from src.camera_mode import camera_mode
 # from src.camera_mode import access_camera
 
 st.set_page_config(page_title="Two-Face-Similarity", page_icon=":🙂:", layout="wide", initial_sidebar_state="expanded")
@@ -29,10 +30,27 @@ if selected_mode == 'Pictures':
             except:
                 st.write('Please select Different Backend Or Model')
 else:
-    st.camera_input(label='Smile Please'
-        # access_camera()
-        )
-    # access_camera()
+    if camera_mode() is not None:
+        img1_path , img2_path = camera_mode()
+
+        if st.button('Show Similarity'):
+            try:
+                sim = Similarity()
+
+                similarity_score = sim.similarity(img1=img1_path,img2=img2_path,backend=selected_backend,model=selected_model)
+                st.title(f'Similarity score: {similarity_score} %')
+            except:
+                st.write('Please select Different Backend Or Model')
+
+                
+
+
+
+
+    # st.camera_input(label='Smile Please'
+    #     # access_camera()
+    #     )
+    # # access_camera()
 
     st.write('camera mode.')
 
--- src/camera_mode.py
+++ src/camera_mode.py
@@ -1,18 +1,28 @@
 
-# import cv2
+import streamlit as st
+from PIL import Image
+import os
+from src.save_img import save_uploaded_img_face1 , save_uploaded_img_face2
 
-# def access_camera():
-#     cap = cv2.VideoCapture(0)
-#     while cap.isOpened(): # this is infinite loop
-#         ret , frame = cap.read()
-#         # frame = cv2.resize(src=frame,dsize=(700,400))
-#         frame = cv2.flip(src=frame,flipCode=1)
-#         cv2.imshow('Video',frame)
-#         k = cv2.waitKey(25)
+def camera_mode():
+    col1 ,col2 = st.columns(2)
+    with col1:
+
+        img1 = st.camera_input('Smile Please',key='Hello')
+
+    with col2:
+        img2 = st.camera_input('Smile Please new',key='HII')
+
+
+    if img1 and img2 is not None:
+        if save_uploaded_img_face1(img1) and save_uploaded_img_face2(img2):
+
+            img1_path =os.path.join('images/uploaded_images/face1',img1.name)
+            img2_path =os.path.join('images/uploaded_images/face2',img2.name)
+
         
-#         if k == ord('x'):
-#             break
-#     cap.release()
-#     cv2.destroyAllWindows()
-
+            return img1_path , img2_path
+        else:
+            return None ,None
+        
 
